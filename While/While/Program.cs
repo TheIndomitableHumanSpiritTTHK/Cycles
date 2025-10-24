09e6168f3ade040309e1d689feb09e383ed87409
Leib92 <opilane@HP-CZC321705Q>
2025-10-24 13:25:38 +0300

    While näidis koos vooskeemiga

---
+++ While/While/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Joonistan sulle kolmnurga. Sisesta kolmnurga suurus");
+int kolmnurgaSuurus = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= kolmnurgaSuurus)
+{
+    Console.WriteLine();
+    int j = 1;
+    while (j <= i)
+    {
+        Console.Write("T");
+        j++;
+    }
+    i++;
+}
