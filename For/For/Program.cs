85427612bf8b97fea01fdff2c1c111a1882f4635
Leib92 <opilane@HP-CZC321705Q>
2025-10-31 13:16:48 +0200

    For ja array näidised

---
+++ For/For/Program.cs
@@ -0,0 +1,26 @@
+﻿////Console.WriteLine("For tsükkel");
+////int omg = 5;
+
+////for (int i = 0; i < omg; i++)
+////{
+////    Console.WriteLine($"omg {i}");
+////}
+
+//string[] objectArray = new string[5];  // 
+//int objectCount = 0;
+//string temp = "no name";
+//while (objectCount < 5)
+//{
+//    Console.WriteLine("Sisesta järgmine lemmiklooma nimi");
+//    temp = Console.ReadLine();
+//    objectArray[objectCount] = temp;
+//    objectCount++;
+//}
+//Console.WriteLine("Oled sisestanud järgnevad loomanimed");
+//for (int i = 0; i < objectArray.Length; i++)
+//{
+//    Console.WriteLine($"{i+1}. Loomanimi on: {objectArray[i]}");
+//}
+
+string[] monsters = {"MrSkeletal", "Frankestein", "Shigeru Myamoto", "Kraat", "Joodik Balti Jaamas"};
+Console.WriteLine(monsters[1]);
